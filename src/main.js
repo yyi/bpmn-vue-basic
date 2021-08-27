7ecf6637051e0657f84bb5e41a975029e6763613
yyi <yyi.mailer@gmail.com>
2021-08-27 09:27:04 +0800

    兼容ie设置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'babel-polyfill'
 Vue.config.productionTip = false
     // 以下为bpmn工作流绘图工具的样式
 import 'bpmn-js/dist/assets/diagram-js.css' // 左边工具栏以及编辑节点的样式
